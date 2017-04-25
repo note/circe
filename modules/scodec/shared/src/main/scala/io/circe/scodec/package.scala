8dac781e056b90f62fbd3b560c9be990db3f3deb
Michal Sitko <pierwszy1@gmail.com>
2017-04-25 16:41:08 +0200

    #551 BitVector encoder/decoder fix

--- modules/scodec/shared/src/main/scala/io/circe/scodec/package.scala
+++ modules/scodec/shared/src/main/scala/io/circe/scodec/package.scala
@@ -2,13 +2,21 @@ package io.circe
 
 import _root_.scodec.bits.{BitVector, ByteVector}
 
+import scala.collection.immutable.StringOps
+import scala.language.implicitConversions
+
 package object scodec {
+  @inline private implicit def augmentString(x: String): StringOps = new StringOps(x)
+
   implicit final val decodeBitVector: Decoder[BitVector] =
     Decoder.instance { c =>
       Decoder.decodeString(c) match {
         case Right(str) =>
-          BitVector.fromBase64Descriptive(str) match {
-            case r @ Right(_) => r.asInstanceOf[Decoder.Result[BitVector]]
+          BitVector.fromBase64Descriptive(str.drop(1)) match {
+            case Right(r) =>
+              val numberOfCharsToIgnore = 8 - str.take(1).toByte
+              val res = r.take(r.size - numberOfCharsToIgnore)
+              Right(res)
             case Left(err) => Left(DecodingFailure(err, c.history))
           }
         case l @ Left(_) => l.asInstanceOf[Decoder.Result[BitVector]]
@@ -16,7 +24,15 @@ package object scodec {
     }
 
   implicit final val encodeBitVector: Encoder[BitVector] =
-    Encoder.encodeString.contramap(_.toBase64)
+    Encoder.encodeString.contramap { bv =>
+      val significantBits = if (bv.isEmpty) {
+        0
+      } else {
+        val mod = bv.size % 8
+        if (mod == 0) 8 else mod
+      }
+      significantBits.toByte + bv.toBase64
+    }
 
   implicit final val decodeByteVector: Decoder[ByteVector] =
     Decoder.instance { c =>
