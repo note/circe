6e56f8668dc46266f2d3ce7e9b9f827ad99fec13
Michal Sitko <pierwszy1@gmail.com>
2017-04-25 16:41:08 +0200

    551 BitVector encoder/decoder fix

--- modules/scodec/shared/src/main/scala/io/circe/scodec/package.scala
+++ modules/scodec/shared/src/main/scala/io/circe/scodec/package.scala
@@ -1,23 +1,72 @@
 package io.circe
 
-import _root_.scodec.bits.{BitVector, ByteVector}
+import _root_.scodec.bits.{ BitVector, ByteVector }
+import scala.collection.immutable.StringOps
 
-package object scodec {
+private[circe] trait BitVectorCodec {
   implicit final val decodeBitVector: Decoder[BitVector] =
     Decoder.instance { c =>
       Decoder.decodeString(c) match {
         case Right(str) =>
-          BitVector.fromBase64Descriptive(str) match {
-            case r @ Right(_) => r.asInstanceOf[Decoder.Result[BitVector]]
-            case Left(err) => Left(DecodingFailure(err, c.history))
+          val richStr = new StringOps(str)
+          numberOfCharsToIgnore(richStr) match {
+            case Some(toIgnore) =>
+              BitVector.fromBase64Descriptive(richStr.drop(1)) match {
+                case Left(err) => Left(DecodingFailure(err, c.history))
+                case Right(r) =>
+                  val res = r.take(r.size - toIgnore)
+                  Right(res)
+              }
+            case None =>
+              Left(DecodingFailure("Incorrect format of BitVector field", c.history))
           }
         case l @ Left(_) => l.asInstanceOf[Decoder.Result[BitVector]]
       }
     }
 
+  /**
+    * For serialization of BitVector we use base64. scodec's implementation of `toBase64` adds padding to 8 bits.
+    * That's not desired in our case and to preserve original BitVector length we prefix base64 representation with
+    * simple one byte long "header" which stands for number of bits of data in the last byte of base64 representation.
+    *
+    * Examples:
+    * encodeBitVector(bin"101")
+    * res: io.circe.Json = "3oA==" // starts with "3' because BitVector is of size 3
+    *
+    * encodeBitVector(bin"")
+    * res: io.circe.Json = "0"
+    *
+    * encodeBitVector(bin"11001100")
+    * res: io.circe.Json = "8zA=="
+    *
+    */
   implicit final val encodeBitVector: Encoder[BitVector] =
-    Encoder.encodeString.contramap(_.toBase64)
+    Encoder.encodeString.contramap { bv =>
+      significantBitsInLastByte(bv).toString + bv.toBase64
+    }
 
+  private def significantBitsInLastByte(bitVector: BitVector): Byte = {
+    if (bitVector.isEmpty) {
+      0
+    } else {
+      val mod = (bitVector.size % 8).toByte
+      if (mod == 0) 8 else mod
+    }
+  }
+
+  private def numberOfCharsToIgnore(input: StringOps): Option[Int] = {
+    input.headOption.flatMap { head =>
+      val digit = (new scala.runtime.RichChar(head)).asDigit
+      if (digit >= 0 && digit <= 8) {
+        Some (8 - digit)
+      } else {
+        None
+      }
+    }
+  }
+}
+
+private[circe] trait ByteVectorCodec {
   implicit final val decodeByteVector: Decoder[ByteVector] =
     Decoder.instance { c =>
       Decoder.decodeString(c) match {
@@ -33,3 +82,5 @@ package object scodec {
   implicit final val encodeByteVector: Encoder[ByteVector] =
     Encoder.encodeString.contramap(_.toBase64)
 }
+
+package object scodec extends ByteVectorCodec with BitVectorCodec
--- modules/tests/shared/src/test/scala/io/circe/scodec/ScodecSuite.scala
+++ modules/tests/shared/src/test/scala/io/circe/scodec/ScodecSuite.scala
@@ -1,14 +1,15 @@
 package io.circe.scodec
 
-import _root_.scodec.bits.{ BitVector, ByteVector }
+import _root_.scodec.bits.{BitVector, ByteVector}
 import cats.Eq
+import io.circe.Json
 import io.circe.testing.CodecTests
 import io.circe.tests.CirceSuite
 import org.scalacheck.Arbitrary
 
 class ScodecSuite extends CirceSuite {
   implicit val arbitraryBitVector: Arbitrary[BitVector] =
-    Arbitrary(Arbitrary.arbitrary[Array[Byte]].map(BitVector.view))
+    Arbitrary(Arbitrary.arbitrary[Iterable[Boolean]].map(BitVector.bits))
 
   implicit val arbitraryByteVector: Arbitrary[ByteVector] =
     Arbitrary(Arbitrary.arbitrary[Array[Byte]].map(ByteVector.view))
@@ -22,4 +23,24 @@ class ScodecSuite extends CirceSuite {
   checkLaws("Codec[BitVector]", CodecTests[BitVector].codec)
   
   checkLaws("Codec[ByteVector]", CodecTests[ByteVector].codec)
+
+  "Codec[ByteVector]" should "return failure in case first character is not from range 0-8" in {
+    val json = Json.fromString("amA==")
+    assert(decodeBitVector.decodeJson(json).isLeft)
+
+    val json2 = Json.fromString("9mA==")
+    assert(decodeBitVector.decodeJson(json2).isLeft)
+  }
+
+  "Codec[ByteVector]" should "return failure in case input is an empty string" in {
+    val json = Json.fromString("")
+    assert(decodeBitVector.decodeJson(json).isLeft)
+  }
+
+  // this test shows that decoder is to some extend liberal
+  // even though such input could not have been produced by BitVector encoder it's getting decoded to empty BitVector
+  "Codec[ByteVector]" should "return empty BitVector in case contains only non-zero header" in {
+    val json = Json.fromString("3")
+    assert(decodeBitVector.decodeJson(json).right.get == BitVector.empty)
+  }
 }
